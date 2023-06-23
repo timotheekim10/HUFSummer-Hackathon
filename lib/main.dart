c77e113ed70316e53650fe870dfddbf99edf2a2b
timotheekim10 <timotheekim10@gmail.com>
2023-06-24 03:45:36 +0900

    지도 재세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,99 @@
+import 'dart:async';
+import 'dart:developer' show log;
 import 'package:flutter/material.dart';
-import 'package:projects/screens/input_page.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await _initialize();
+  runApp(const NaverMapApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+      clientId: '7im6q2ce1m',
+      onAuthFailed: (ex) => log("********* 네이버맵 인증오류 : $ex *********"));
+}
+
+class NaverMapApp extends StatelessWidget {
+  final int? testId;
+
+  const NaverMapApp({super.key, this.testId});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) => MaterialApp(
+      home: testId == null
+          ? const FirstPage()
+          : TestPage(key: Key("testPage_$testId")));
 }
 
-class _MyAppState extends State<MyApp> {
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(),
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: InputPage(),
-            ),
-          ),
-        )
+    return Scaffold(
+        appBar: AppBar(title: const Text('First Page')),
+        body: Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TestPage()));
+                },
+                child: const Text('Go to Second Page'))));
+  }
+}
+
+class TestPage extends StatefulWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  @override
+  State<TestPage> createState() => TestPageState();
+}
+
+class TestPageState extends State<TestPage> {
+  late NaverMapController _mapController;
+  final Completer<NaverMapController> mapControllerCompleter = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final pixelRatio = mediaQuery.devicePixelRatio;
+    final mapSize =
+    Size(mediaQuery.size.width - 32, mediaQuery.size.height - 72);
+    final physicalSize =
+    Size(mapSize.width * pixelRatio, mapSize.height * pixelRatio);
+
+    print("physicalSize: $physicalSize");
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF343945),
+      body: Center(
+          child: SizedBox(
+              width: mapSize.width,
+              height: mapSize.height,
+              // color: Colors.greenAccent,
+              child: _naverMapSection())),
     );
   }
+
+  Widget _naverMapSection() => NaverMap(
+    options: const NaverMapViewOptions(
+        initialCameraPosition: NCameraPosition(
+            target: NLatLng(0, 0),
+            zoom: 10,
+            bearing: 0,
+            tilt: 0
+        ),
+        indoorEnable: true,
+        locationButtonEnable: false,
+        consumeSymbolTapEvents: false),
+    onMapReady: (controller) async {
+      _mapController = controller;
+      mapControllerCompleter.complete(controller);
+      log("onMapReady", name: "onMapReady");
+    },
+  );
 }
--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
 
 class MapPage extends StatefulWidget {
   final int budget;
@@ -12,28 +14,26 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  NaverMapController? _controller;
+
   void _showBottomSheet() {
     showModalBottomSheet(
       context: context,
       builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 40),
-          child: Container(
-            height: 200,
-            color: Colors.green,
-            child: Center(
-              child: Text(
-                '식당이름',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
+        return Container(
+          height: 200,
+          color: Colors.green,
+          child: Center(
+            child: Text(
+              'Hello Bottom Sheet',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
               ),
             ),
           ),
         );
       },
-      barrierColor: Colors.transparent,
     );
   }
 
@@ -61,6 +61,23 @@ class _MapPageState extends State<MapPage> {
                     color: Colors.white,
                   ),
                 ),
+              ),
+            ),
+            Expanded(
+              child: NaverMap(
+                options: NaverMapViewOptions(
+                  initialCameraPosition: NCameraPosition(
+                      target: NLatLng(0, 0),
+                      zoom: 10,
+                      bearing: 0,
+                      tilt: 0
+                  ),
+                ),
+                onMapReady: (controller) {
+                  setState(() {
+                    _controller = controller;
+                  });
+                },
               ),
             ),
           ],
