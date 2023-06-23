d0fd04ee27b870f89defbaf3658859ef568546c4
timotheekim10 <timotheekim10@gmail.com>
2023-06-24 05:58:22 +0900

    지도 정상작동

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,50 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:projects/screens/input_page.dart';
-
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(),
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: InputPage(),
-            ),
-          ),
-        )
+      debugShowCheckedModeBanner: false,
+      title: 'Naver Map',
+      home: NaverMapTest(),
     );
+  }
+}
+
+class NaverMapTest extends StatefulWidget {
+  @override
+  _NaverMapTestState createState() => _NaverMapTestState();
+}
+
+class _NaverMapTestState extends State<NaverMapTest> {
+  Completer<NaverMapController> _controller = Completer();
+  MapType _mapType = MapType.Basic;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('NaverMap Test')),
+      body: Container(
+        child: NaverMap(
+          onMapCreated: onMapCreated,
+          mapType: _mapType,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.5665, 126.9780), // 서울의 위도(latitude)와 경도(longitude) 좌표
+            zoom: 13.0, // 초기 줌 레벨
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
   }
 }
